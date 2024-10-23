46177177c216b56e2f20f513144ef821b2c4cb49
matthieu <matthieu@akawam.com>
2024-10-23 13:57:27 +0200

    init tailwind

---
+++ frontend/wp-content/themes/trust2give/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style/app.css">
+  <script src="script.js"></script>
+</head>
+<body>
+tfh
+</body>
+</html>
