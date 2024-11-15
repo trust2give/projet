209f974ec71cae0c9e67ee706673f7bdfdb32b4c
trust2give <admin@trust2give.net>
2024-11-15 22:14:48 +0100

    delete plugin

--- frontend/wp-content/plugins/web3-authentication/classes/common/Web3/resources/js/web3/walletconnect/dist/_types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { WalletConnectModalSign } from './src/client';
-export type { WalletConnectModalEventCallback, WalletConnectModalSignConnectArguments, WalletConnectModalSignDisconnectArguments, WalletConnectModalSignOptions, WalletConnectModalSignRequestArguments, WalletConnectModalSignSession } from './src/client';
