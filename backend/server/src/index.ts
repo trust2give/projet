1eba6987e7e88acccacd56f45315bfa9d0a3c366
Franck Dervillez <franck.dervillez@melliades-consulting.com>
2025-01-25 15:38:58 +0100

    Backend 13

--- backend/server/src/index.ts
+++ backend/server/src/index.ts
@@ -1,7 +1,12 @@
+const hre = require("hardhat");
+import { Address, encodeAbiParameters, decodeAbiParameters } from 'viem'
 import express from 'express';
 import userRoutes from './routes/userRoutes';
 import cors from 'cors';
-import { initState } from './logic/states';
+import { accountRefs, initState, addAccount, accountType, globalState } from "./logic/states";
+import { readLastContractSetJSONfile, readLastDiamondJSONfile } from "./libraries/files";
+import { contractSet, diamondNames, facetNames, smart, smartEntry, encodeInterfaces } from "./T2G_Data";
+import { contractRecord, rwRecord, rwType, menuRecord, Account, NULL_ADDRESS, regex, regex2, regex3 } from "./libraries/types";
 
 const app = express();
 const PORT = 8080;
@@ -12,11 +17,89 @@ app.use(cors());
 app.use(express.json()); // Middleware pour parser le JSON
 app.use('/T2G', userRoutes); // Utilisation des routes des utilisateurs
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   
     // Initialize globalState variable (Warning : async / wait not possible so wallet / public clients deactivated)
     // to be changed
-    initState();
+  initState();
+
+  var initialized : Boolean = false;
+  type accKeys = keyof typeof accountRefs;
+
+  if (await readLastDiamondJSONfile()) {
+
+    try {            
+        const getRoot = await hre.viem.getContractAt( 
+            diamondNames.Diamond.name, 
+            diamondNames.Diamond.address
+            );
+
+        initialized = await addAccount( 
+            10, 
+            diamondNames.Diamond.name, 
+            diamondNames.Diamond.address, 
+            await getRoot.read.wallet_T2G_root( 
+                [], 
+                accountRefs[<accKeys>`@0`].client 
+                ) //root 
+            );        
+        }
+    catch (error) {
+        console.error(">> Error :: No T2G_Root initialized @ %s", diamondNames.Diamond.address, error)
+        }
+
+    if (await readLastContractSetJSONfile()) {
+        try {
+            await addAccount( 
+                11, 
+                contractSet[0].name, 
+                contractSet[0].address, 
+                [] 
+                );
+            }
+        catch (error) {
+            console.error(">> Error :: No StableCoin Contract initialized @ %s ", contractSet[0].address, error)
+            }
+        }
+    
+    if (initialized) {
+        var rank = 12;
+        type AccountKeys = keyof typeof Account;
+        const indice = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (const facet of facetNames) {
+            if (smart.findIndex((item) => item.contract == facet.name) > -1) {
+
+                const getAddr = await hre.viem.getContractAt( 
+                    facet.name, 
+                    diamondNames.Diamond.address
+                    );
+
+                try {    
+
+                    const get : Address = (facet.get) ? await getAddr.read[<string>facet.get]( 
+                        [], 
+                        accountRefs[<accKeys>`@0`].client 
+                        ) : NULL_ADDRESS;                
+                                            
+                    await addAccount( 
+                        rank, 
+                        facet.name, 
+                        get, 
+                        (facet.wallet) ? await getAddr.read[<string>facet.wallet]( 
+                            [], 
+                            accountRefs[<accKeys>`@0`].client 
+                            ) : [] 
+                        );
+    
+                    rank++;
+                    }
+                catch (error) {
+                    console.error(">> Error :: No Facet Contract initialized %s, @ %s", facet.name, diamondNames.Diamond.address)
+                   }
+                }
+            }
+        } 
+    }
   
   console.log(`Serveur en écoute sur le port ${PORT}`);
 });
---
+++ backend/server/src/libraries/files.ts
@@ -0,0 +1,89 @@
+import { Address } from "viem";
+import fs from 'fs';
+import { diamondNames, contractSet } from "../T2G_Data";
+import { contractRecord, diamondCore, Account, NULL_ADDRESS, regex, regex2, regex3 } from "./types";
+
+
+export interface facetRecord { 
+    diamond: Address, 
+    facets: Object
+    }
+
+export async function readLastContractSetJSONfile() : Promise<boolean> {
+    const jsonString = fs.readFileSync('./scripts/ContractSet.json', 'utf-8');
+    const item : contractRecord = JSON.parse(jsonString);
+    if (item.name != "EUR") return false;
+    //colorOutput("Recall Last EUR Smart Contract Record >> ".concat(JSON.stringify(item)), "cyan");
+    contractSet[0] = <contractRecord>item;
+    return true;
+    }
+/*
+export async function writeLastContractJSONfile( ) {
+    let JsonFile = JSON.stringify(contractSet[0]);
+    //colorOutput("Save last EUR Contract Record >> ".concat(JSON.stringify(contractSet[0])), "cyan");
+    fs.writeFile('./scripts/ContractSet.json', JsonFile, (err) => {
+        if (err) {
+            console.log('Error writing file:', err);
+        } else {
+            console.log('Successfully wrote file');
+            }
+        });
+    }
+*/
+/*
+export async function readLastFacetJSONfile( facetName: string, diamond: Address ) : Promise<Address> {
+    const jsonString = fs.readFileSync('./scripts/T2G_Facets.json', 'utf-8');
+    const FacetList : facetRecord = JSON.parse(jsonString);
+    if (FacetList.diamond != diamond) throw("Bad Record Name for Diamond Address recovery :: ".concat(FacetList.diamond));
+    if (!(facetName in FacetList.facets)) throw("Bad Record Name for Facet Address recovery :: ".concat(facetName));
+    return FacetList.facets[facetName];
+    }
+
+export async function writeLastFacetJSONfile( facets: Object, diamond: Address ) {
+    const jsonString = fs.readFileSync('./scripts/T2G_Facets.json', 'utf-8');
+    const FacetRecord : facetRecord = JSON.parse(jsonString);
+    if (FacetRecord.diamond != diamond) {
+        if (Object.keys(facets).length == 0) FacetRecord.diamond = diamond;
+        else throw("Bad Record Name for Diamond Address recovery :: ".concat(FacetRecord.diamond));
+        }
+
+    for ( const facet of Object.keys(facets)) {
+        FacetRecord.facets[facet] = facets[facet];
+        }
+
+    let JsonFile = JSON.stringify(FacetRecord);
+    fs.writeFile('./scripts/T2G_Facets.json', JsonFile, (err) => {
+        if (err) {
+            console.log('Error writing file:', err);
+        } else {
+            console.log('Successfully wrote file');
+            }
+        });
+    }
+*/
+
+export async function writeLastDiamondJSONfile( ) {
+    let JsonFile = JSON.stringify(diamondNames);
+    fs.writeFile('./scripts/T2G_Root.json', JsonFile, (err) => {
+        if (err) {
+            console.log('Error writing file:', err);
+        } else {
+            console.log('Successfully wrote file');
+            }
+        });
+    }
+      
+export async function readLastDiamondJSONfile() : Promise<boolean> {
+    const jsonString = fs.readFileSync('./T2G_Root.json', 'utf-8');
+    const DiamondCore : diamondCore = JSON.parse(jsonString);
+
+    if (DiamondCore.Diamond.name != "T2G_root") return false;
+    if (!DiamondCore.Diamond.address.match(regex)) return false;
+    
+    diamondNames.Diamond = <contractRecord>DiamondCore.Diamond;
+    diamondNames.DiamondInit = <contractRecord>DiamondCore.DiamondInit;
+    diamondNames.DiamondCutFacet = <contractRecord>DiamondCore.DiamondCutFacet;
+    diamondNames.DiamondLoupeFacet = <contractRecord>DiamondCore.DiamondLoupeFacet;
+
+    return true;
+    }
