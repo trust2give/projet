5984e5daf8ea5f75629631a5fdf5825c2b0d7ffb
Franck Dervillez <franck.dervillez@melliades-consulting.com>
2025-01-25 11:23:55 +0100

    Test3

--- backend/server/src/routes/userRoutes.ts
+++ backend/server/src/routes/userRoutes.ts
@@ -1,10 +1,10 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import { getUserById, addUser } from '../services/userService';
 
 const router = Router();
 
-// Route GET pour récupérer tous les utilisateurs
-router.get('/:id', (req, res) => {
+// Route GET pour récupérer un utilisateur par ID
+router.get('/:id', (req: Request, res: Response) => {
     const userId = parseInt(req.params.id);
     const user = getUserById(userId);
   
@@ -14,9 +14,9 @@ router.get('/:id', (req, res) => {
       return res.status(404).json({ message: 'Utilisateur non trouvé' });
     }
   });
-
-// Route POST pour ajouter un nouvel utilisateur
-router.post('/', (req, res) => {
+  
+  // Route POST pour ajouter un nouvel utilisateur
+  router.post('/', (req: Request, res: Response) => {
     const newUser = req.body;
     const addedUser = addUser(newUser);
     res.status(201).json(addedUser);
