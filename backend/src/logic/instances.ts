3649e8ae0112060d963dea4052e7d10c4af4d95b
Franck Dervillez <franck.dervillez@melliades-consulting.com>
2025-02-03 22:27:34 +0100

    Instance 2

--- backend/src/logic/instances.ts
+++ backend/src/logic/instances.ts
@@ -1,8 +1,16 @@
 import { dataDecodeABI, abiData, honeyFeatures, pollenFeatures, Typeoftoken, Statusoftoken } from "../interface/types";
 import { contractRecord, rwRecord, rwType, menuRecord, Account, NULL_ADDRESS, regex, regex2, regex3 } from "../libraries/types";
+import fs from 'fs';
 
 export const showInstance = async ( contract : contractRecord ) : Promise<Array<any>> => {
-    return contract.abi.file.abi.map( 
+    console.log(contract)
+
+    const jsonFacet = fs.readFileSync( 
+        contract.abi.path, 
+        'utf-8'
+        );
+    
+    return JSON.parse(jsonFacet).abi.map( 
         (item : { 
             inputs: Array<abiData>, 
             name: string, 
