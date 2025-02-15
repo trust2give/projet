977ef9822dec6e9908d4f251dd96d0683c86a6fd
Franck Dervillez <franck.dervillez@melliades-consulting.com>
2025-02-15 14:17:27 +0100

    Debug 29

--- backend/src/logic/honey.ts
+++ backend/src/logic/honey.ts
@@ -140,7 +140,7 @@ export const fundCallback : callbackType[] = [
     help: "fund | set [ { from: <Account>, value: <number>, rate: <number> }] -> Create a new fund and return the Id <hash>",
     callback: async ( inputs: Array<{ from?: Account, value?: number, rate?: number }> ) : Promise<Object[] | undefined> => {
 
-        console.log( "fund set", inputs, typeof inputs[0].from )
+        console.log( "fund set", inputs, typeof inputs[0].from, Object.keys(accountRefs) )
 
         if (inputs.length == 0) return undefined;
 
@@ -152,7 +152,6 @@ export const fundCallback : callbackType[] = [
         if (gwei == undefined) return undefined;
         
         const toAccount = (<accountType>accountRefs[Account.AA]);
-        
 
         for ( const input of inputs) {
             var tx1 : typeof regex2 = NULL_HASH;
